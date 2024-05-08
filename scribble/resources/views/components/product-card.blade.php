2a7e9ce45c7fb9ff843181e579f77fa89cce371a
Joxnhby <114238028+Joxnhby@users.noreply.github.com>
2024-05-08 11:32:59 +0700

    product card

---
+++ scribble/resources/views/components/product-card.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link href="/css/variable.css" rel="stylesheet">
+    <link href="/css/product-card.css" rel="stylesheet">
+</head>
+<body>
+    <a class="card">
+        <div class="product-img">
+            <img src="/images/pict1.jpg">
+        </div>
+        <hr class="divider">
+        <div class="product-details">
+            <div class="title-wish">
+                <h3 class="product-title">Sinar Dunia Buku Tulis 38 lembar</h3>
+                <div class="wish">
+                    <img src="/icons/love-outline.svg">
+                </div>
+            </div>
+            <div class="rating-sell">
+                <div class="rating">
+                    <img src="/icons/star.svg">
+                    <img src="/icons/star.svg">
+                    <img src="/icons/star.svg">
+                    <img src="/icons/star.svg">
+                    <img src="/icons/star.svg">
+                </div>
+                <div class="sell">
+                    <h5>100 Terjual</h5>
+                </div>
+            </div>
+            <div class="price">
+                <h4>Rp. 35.000,00</h4>
+            </div>
+        </div>
+    </a>
+</body>
+</html>
