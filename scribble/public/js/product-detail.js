a7745e289c30409d9a8a832bd0471971f1f59b5a
MarleneJusup <127584520+MarleneJusup@users.noreply.github.com>
2024-05-14 20:00:27 +0700

    product-detail-ongoing2

---
+++ scribble/public/js/product-detail.js
@@ -0,0 +1,6 @@
+const dropdownSortBtn = document.querySelector(".dropdown-sort");
+const dropdownContent = document.querySelector(".dropdown-sort-content");
+
+dropdownSortBtn.addEventListener("click", function () {
+    dropdownSortBtn.classList.toggle("active");
+});
