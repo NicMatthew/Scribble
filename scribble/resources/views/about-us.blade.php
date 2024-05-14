3120f2e7cefdac6c6360085bbc5a1a57415cec36
jesslyntanu <129813528+jesslyntanu@users.noreply.github.com>
2024-05-14 16:11:13 +0700

    about us half way

--- scribble/resources/views/about-us.blade.php
+++ scribble/resources/views/about-us.blade.php
@@ -5,12 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Scribble | About Us</title>
     <link href="/css/variable.css" rel="stylesheet">
-    <link href="/css/terms.css" rel="stylesheet">
+    <link href="/css/about-us.css" rel="stylesheet">
 </head>
 <body>
     @include('components/navbar')
+    <div class="container">
+        <div class="first-section">
+            <div class="text1">
+                <p class="h2">We believe that stationery is <mark id='orange'>more</mark> than just supplies.</p>
+            </div>
 
-    
+            <div class="text2">
+                <p class="h2">it's a way to <mark id='green'>spark creativity</mark> and make <mark id='blue'>your mark</mark> on the world.</p>
+            </div>
+        </div>
+
+        <div class="second-section">
+            <div class="left">
+                <p class="b3">Scribble is a leading online provider of exceptional quality stationery designed to inspire and empower individuals across various endeavors. </p>
+                <p class="b3">we curate a comprehensive collection encompassing a diverse range of products, from school, office to profesional supplies. Whether you are a seasoned professional seeking to enhance your productivity or a budding artist yearning to express your creativity, our meticulously selected stationery caters to your unique needs. </p>
+            </div>
+            <p class="h2">Who We Are?</p>
+        </div>
+
+        <div class="third-section">
+            <h1>Our Accomplishments</h1>
+            <div class="up">
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+
+
+
     @include('components/footer')
     
 </body>
