f1a62c4f9657b538dd21bc2b1c62a662ae630b85
MarleneJusup <127584520+MarleneJusup@users.noreply.github.com>
2024-05-15 13:26:39 +0700

    addres_mungkindone

--- scribble/resources/views/address.blade.php
+++ scribble/resources/views/address.blade.php
@@ -5,16 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="/css/variable.css" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/4d9121ebec.js" crossorigin="anonymous"></script>
+    <!-- <script src="https://kit.fontawesome.com/4d9121ebec.js" crossorigin="anonymous"></script> -->
     <link href="/css/address.css" rel="stylesheet">
-    <script src="/js/address.js"></script>
+    <script src="/js/address.js" defer=""></script>
 </head>
 <body>
     <div class="big-container">
         <div class="header">
-            <img src="/icons/prev.svg" style="width: 40px; height: 40px" alt="">
+            <img src="/icons/prev.svg" style="width: 40px; height: 40px; cursor:pointer;" alt="">
             <p class="h2">Address</p>
-            <img src="/icons/plus-address.svg" style="width: 40px; height: 40px" alt="">
+            <img src="/icons/plus-address.svg" style="width: 40px; height: 40px; cursor:pointer;" alt="">
         </div>
 
         <hr class="line">
@@ -27,8 +27,8 @@
                 <p style="color: var(--dark-grey); font-size: 18px; margin-bottom: 5px">Jl. Pakuan No. 5, Kelurahan Sumur Batu, Kecamatan Babakan Madang, Kabupaten Bogor, Jawa Barat 16810</p>
             </div>
             <div class="right-address">
-                <img src="/icons/exit-address.svg" style="width: 55px; height: 55px" alt="">
-                <img src="/icons/next.svg" style="width: 55px; height: 55px" alt="">
+                <img src="/icons/exit-address.svg" style="width: 55px; height: 55px; cursor:pointer;" alt="">
+                <img src="/icons/next.svg" style="width: 55px; height: 55px; cursor:pointer;" alt="">
             </div>
         </div>
 
@@ -58,33 +58,35 @@
             </div>
         </div>
         
-        <div class="overlay">
-            <p class="b1" style="font-weight: bold; margin-bottom:20px">New Address</p>
-            <div class="form-address">
-                 <div class=input-box>
-                    <p>Address Name</p>
-                    <input type="text" placeholder="Address Name" required>
+        <div class="modal" id="modal">
+            <div class="overlay">
+                <p class="b1" style="font-weight: bold; margin-bottom:15px; display:flex; align-items: center; justify-content: center">New Address</p>
+                <div class="form-address">
+                    <div class=input-box>
+                        <p>Address Name</p>
+                        <input type="text" placeholder="Address Name" required>
+                    </div>
+                    <div class=input-box>
+                        <p>Recipient Name</p>
+                        <input type="text" placeholder="Recipient Name" required>
+                    </div>
+                    <div class=input-box>
+                        <p>Phone Number</p>
+                        <input type="text" placeholder="Phone Number" required>
+                    </div>
+                    <div class=input-box>
+                        <p>Full Address</p>
+                        <input type="text" placeholder="Full Address" required>
+                    </div>
+                    <div class=input-box>
+                        <p>Post Code</p>
+                        <input type="text" placeholder="Post Code" required>
+                    </div>
+                    <button type="submit" class="btn">Add Address</button>
+                    <button type="submit" class="cancel">Cancel</button>
                 </div>
-                <div class=input-box>
-                    <p>Recipient Name</p>
-                    <input type="text" placeholder="Recipient Name" required>
-                </div>
-                <div class=input-box>
-                    <p>Phone Number</p>
-                    <input type="text" placeholder="Phone Number" required>
-                </div>
-                <div class=input-box>
-                    <p>Full Address</p>
-                    <input type="text" placeholder="Full Address" required>
-                </div>
-                <div class=input-box>
-                    <p>Post Code</p>
-                    <input type="text" placeholder="Post Code" required>
-                </div>
-                <button type="submit" class="btn">Submit</button>
             </div>
-            </div>
-           
+        </div>
     </div>
 </body>
 </html>
