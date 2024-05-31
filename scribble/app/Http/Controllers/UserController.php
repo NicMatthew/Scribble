e3d0b7da184a0254a8e0067515e4db672e72b792
MarleneJusup <127584520+MarleneJusup@users.noreply.github.com>
2024-05-31 09:36:21 +0700

    edit password

--- scribble/app/Http/Controllers/UserController.php
+++ scribble/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\User;
 
 class UserController extends Controller
@@ -23,25 +24,23 @@ class UserController extends Controller
 
     public function updateProfile(Request $request)
     {
-        // // Validasi data
-        // $request->validate([
-        //     'name' => 'required|string|max:255',
-        //     'dob' => 'required|date',
-        //     'phoneNumber' => 'required|string|max:255',
-        //     'email' => 'required|string|email|max:255',
-        //     'password' => 'required|string|min:8',
-        //     // tambahkan aturan validasi lainnya sesuai kebutuhan
-        // ]);
-
         $user = Auth::user();
 
-        $user->update([
+        // $user->update([
+        //     'NameUser' => $request->name,
+        //     'DOBUser' => $request->dob,
+        //     'PhoneNumberUser' => $request->phoneNumber,
+        //     'EmailUser' => $request->email,
+        //     'password' => bcrypt($request->password),
+        // ]);
+
+        $userData = [
             'NameUser' => $request->name,
             'DOBUser' => $request->dob,
             'PhoneNumberUser' => $request->phoneNumber,
             'EmailUser' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
+            'password' => Hash::make($request->password)
+        ];
 
         // Cek apakah ada file gambar yang diunggah
         if ($request->hasFile('image')) {
@@ -56,8 +55,12 @@ class UserController extends Controller
             $userData['ImageUser'] = 'data:'.$image->getClientMimeType().';base64,'.$imageBase64;
         }
 
-        // Lakukan pembaruan profil
-        $user->update($userData);
+        // // Lakukan pembaruan profil
+        // $user->update($userData);
+        
+        if (!empty($userData)) {
+            $user->update($userData);
+        }
 
         return redirect('/profile');
     }
