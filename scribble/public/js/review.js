fc11b15b6fbf407917cdd049d64b1df0b9b72f21
NicMatthew <113076301+NicMatthew@users.noreply.github.com>
2024-07-19 15:03:55 +0700

    review half finish

--- scribble/public/js/review.js
+++ scribble/public/js/review.js
@@ -21,30 +21,48 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.getElementById("submit-button").addEventListener('click', function () {
-    // document.forms["form-submit"].submit();
+    var check = 0;
+    textreview = document.querySelectorAll(".review-text");
+    rating = document.querySelectorAll(".Rating-input");
+    image = document.querySelectorAll(".Image-input");
+    // console.log(textreview)
+    rating.forEach((element) => {
+        if (element.value == '') {
+            check++;
+        }
+    });
+    
+    if (check != 0) {
+        alert('Please fill all review form')
+    } else {
+        alert('mantap')
+        // document.forms["form-submit"].submit();
+    }
+    
 })
 
 document.addEventListener("DOMContentLoaded", function () {
-    var input = document.getElementById("upload-photo");
-    var preview = document.getElementById("photo-preview");
-    var removeBtn = document.getElementById("remove-photo");
+    document.querySelectorAll(".upload-input").forEach((input, index) => {
+        var preview = document.getElementById(`photo-preview-${index}`);
+        var removeBtn = document.getElementById(`remove-photo-${index}`);
 
-    input.addEventListener("change", function () {
-        var file = this.files[0];
-        if (file) {
-            var reader = new FileReader();
-            reader.onload = function (event) {
-                preview.src = event.target.result;
-            };
-            reader.readAsDataURL(file);
-            preview.parentElement.style.display = "flex"; // Tampilkan container preview
-        }
-    });
+        input.addEventListener("change", function () {
+            var file = this.files[0];
+            if (file) {
+                var reader = new FileReader();
+                reader.onload = function (event) {
+                    preview.src = event.target.result;
+                };
+                reader.readAsDataURL(file);
+                preview.parentElement.style.display = "flex"; // Tampilkan container preview
+            }
+        });
 
-    removeBtn.addEventListener("click", function () {
-        preview.src = ""; // Hapus gambar dari tampilan
-        input.value = ""; // Kosongkan nilai input file
-        preview.parentElement.style.display = "none"; // Sembunyikan container preview
+        removeBtn.addEventListener("click", function () {
+            preview.src = ""; // Hapus gambar dari tampilan
+            input.value = ""; // Kosongkan nilai input file
+            preview.parentElement.style.display = "none"; // Sembunyikan container preview
+        });
     });
 });
 
