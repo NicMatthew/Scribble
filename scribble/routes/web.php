3408fad3ae0d52b20e9f0da6f9db9255f7573df2
stefanieangeline <127189827+stefanieangeline@users.noreply.github.com>
2024-07-23 20:32:31 +0700

    middleware

--- scribble/routes/web.php
+++ scribble/routes/web.php
@@ -31,56 +31,60 @@ Route::get("/log-in", [SigninController::class, "login"])->name("log-in");
 Route::post("/log-in", [SigninController::class, "authenticate"])->name("authenticate");
 Route::post("/log-out", [SigninController::class, "logout"])->name("log-out");
 
-Route::get('/profile', [UserController::class, 'showProfile'])->name("profile")->middleware('auth');
-Route::get('/profile/edit', [UserController::class, 'editProfile'])->middleware('auth')->name("edit-profile");
-Route::post('/profile/update', [UserController::class, 'updateProfile'])->middleware('auth')->name('update-profile');
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::middleware(['user'])->group(function () {
 
-Route::get('/transaction-list',[TransactionListController::class,'index'])->name('transaction-list');
-Route::get('/transaction-list/delete',[TransactionListController::class,'transaction_delete'])->name('transaction-delete');
-Route::get('/transaction-list/review',[TransactionListController::class,'transaction_review'])->name('transaction-review');
-Route::post('/transaction-list/review/add',[TransactionListController::class,'transaction_review_add'])->name('transaction-review-add');
+    Route::get('/profile', [UserController::class, 'showProfile'])->name("profile")->middleware('auth');
+    Route::get('/profile/edit', [UserController::class, 'editProfile'])->middleware('auth')->name("edit-profile")->middleware('auth');
+    Route::post('/profile/update', [UserController::class, 'updateProfile'])->middleware('auth')->name('update-profile')->middleware('auth');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/product-catalog', [ProductController::class, 'showAllProducts'])->name('product-catalog');
-Route::get('/product-catalog/{productID}', [ProductController::class, 'showProductDetail'])->name('product-detail');
+    Route::get('/transaction-list',[TransactionListController::class,'index'])->name('transaction-list')->middleware('auth');
+    Route::get('/transaction-list/delete',[TransactionListController::class,'transaction_delete'])->name('transaction-delete')->middleware('auth');
+    Route::get('/transaction-list/review',[TransactionListController::class,'transaction_review'])->name('transaction-review')->middleware('auth');
+    Route::post('/transaction-list/review/add',[TransactionListController::class,'transaction_review_add'])->name('transaction-review-add')->middleware('auth');
 
-Route::post("/addresses/find", [AddressController::class, "findAddress"])->name("find-address");
-Route::get('/addresses/{userId}', [UserController::class, 'showAddresses'])->name('show-addresses');
-Route::post('/add/address', [AddressController::class, 'addAddress'])->name('add-address');
-Route::delete('/address', [AddressController::class, 'deleteAddress'])->name('delete-address');
-Route::post('/editAddress', [AddressController::class, 'editAddress'])->name('edit-address');
+    Route::get('/product-catalog', [ProductController::class, 'showAllProducts'])->name('product-catalog');
+    Route::get('/product-catalog/{productID}', [ProductController::class, 'showProductDetail'])->name('product-detail');
 
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add');
-Route::put('/cart/{product_id}/{variant_id}/{user_id}', [CartController::class, 'update'])->name('cart.update');
+    Route::post("/addresses/find", [AddressController::class, "findAddress"])->name("find-address")->middleware('auth');
+    Route::get('/addresses/{userId}', [UserController::class, 'showAddresses'])->name('show-addresses')->middleware('auth');
+    Route::post('/add/address', [AddressController::class, 'addAddress'])->name('add-address')->middleware('auth');
+    Route::delete('/address', [AddressController::class, 'deleteAddress'])->name('delete-address')->middleware('auth');
+    Route::post('/editAddress', [AddressController::class, 'editAddress'])->name('edit-address')->middleware('auth');
 
-Route::get('/shipping', [ShippingController::class, "index"])->name('shipping.index');
-Route::get('/buy-now', [ShippingController::class, 'buyNow'])->name('buy.now');
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index')->middleware('auth');
+    Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add')->middleware('auth');
+    Route::put('/cart/{product_id}/{variant_id}/{user_id}', [CartController::class, 'update'])->name('cart.update')->middleware('auth');
 
-Route::get('/payment-qr', [PaymentController::class, "qr"])->name("payment");
-Route::post('/payment/successful', [PaymentController::class, "paymentSuccess"])->name("paymentSuccess");
+    Route::get('/shipping', [ShippingController::class, "index"])->name('shipping.index')->middleware('auth');
+    Route::get('/buy-now', [ShippingController::class, 'buyNow'])->name('buy.now')->middleware('auth');
 
-// Route::get('/order-admin', [OrderAdminController::class, 'index']);
-// Route::post('/order-admin/update/{id}', [OrderAdminController::class, 'updateStatus'])->name("order-admin-update");
+    Route::get('/payment-qr', [PaymentController::class, "qr"])->name("payment")->middleware('auth');
+    Route::post('/payment/successful', [PaymentController::class, "paymentSuccess"])->name("paymentSuccess")->middleware('auth');
 
-Route::get('/wishlist', [ProductController::class, 'showWishlist'])->name('wishlist-index');
+    Route::get('/wishlist', [ProductController::class, 'showWishlist'])->name('wishlist-index')->middleware('auth');
 
-Route::post('/wishlist-toggle', [ProductController::class, 'toggleWishlist'])->name('wishlist-toggle');
+    Route::post('/wishlist-toggle', [ProductController::class, 'toggleWishlist'])->name('wishlist-toggle')->middleware('auth');
 
-// //Product Admin Routing
-// Route::get("/product-admin", [AdminController::class, "product_admin"])->name("product_admin");
-// Route::get("/product-admin/find", [AdminController::class, "product_find"])->name("product-find");
-// Route::post("/product-admin/edit", [AdminController::class, "product_edit"])->name("product-edit");
-// Route::post("/product-admin", [AdminController::class, "product_add"])->name("product_add");
-// Route::post('/product-admin/update', [AdminController::class, "product_update"])->name('product_update');
-// Route::get('/product-admin/delete', [AdminController::class, "product_delete"])->name('product_delete');
+    Route::post("/make-order", [PaymentController::class, "makeOrder"])->name("make-order")->middleware('auth');
+    Route::get("/orders/{transactionID}/retry-payment", [PaymentController::class, 'retryPayment'])->name('retry-payment')->middleware('auth');
 
-// //Dashboard Admin Routing
-// Route::get('/dashboard', [AdminController::class, "dashboard"])->name("admin-dashboard");
-// Route::get('/dashboard/discount/add', [AdminController::class, "addDiscount"])->name("add-discount");
-// Route::get('/dashboard/discount/delete', [AdminController::class, "deleteDiscount"])->name("delete-discount");
-// Route::post('/dashboard/banner/store', [AdminController::class, "storeBanner"])->name("admin-store-banner");
-// Route::get('/dashboard/banner/delete/{id}', [AdminController::class, "removeBanner"])->name("admin-remove-banner");
+    Route::get('/terms', function () {
+        return view('terms');
+    });
+    
+    Route::get('/about-us', function () {
+        return view('about-us');
+    });
+    
+    
+    Route::get('/faq', function () {
+        return view('faq');
+    });
+
+});
+
+
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/order-admin', [OrderAdminController::class, 'index']);
     Route::post('/order-admin/update/{id}', [OrderAdminController::class, 'updateStatus'])->name("order-admin-update");
@@ -99,20 +103,4 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/dashboard/discount/delete', [AdminController::class, "deleteDiscount"])->name("delete-discount");
     Route::post('/dashboard/banner/store', [AdminController::class, "storeBanner"])->name("admin-store-banner");
     Route::get('/dashboard/banner/delete/{id}', [AdminController::class, "removeBanner"])->name("admin-remove-banner");
-});
-
-Route::post("/make-order", [PaymentController::class, "makeOrder"])->name("make-order");
-Route::get("/orders/{transactionID}/retry-payment", [PaymentController::class, 'retryPayment'])->name('retry-payment');
-
-Route::get('/terms', function () {
-    return view('terms');
-});
-
-Route::get('/about-us', function () {
-    return view('about-us');
-});
-
-
-Route::get('/faq', function () {
-    return view('faq');
 });
