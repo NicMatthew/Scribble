b170e2987d19991d6666cb058cba95b1f8dba33b
NicMatthew <113076301+NicMatthew@users.noreply.github.com>
2024-08-05 08:15:55 +0700

    p

--- scribble/public/js/product-card.js
+++ scribble/public/js/product-card.js
@@ -1,18 +1,3 @@
-// var wishBtns = document.querySelectorAll(".wish")
-// var urlInput = document.getElementById("urlInput")
-
-// wishBtns.forEach((btn)=>{
-//     btn.addEventListener("click", (e)=>{
-//         if (btn.firstElementChild.src.includes("love-outline.svg")) {
-//             btn.firstElementChild.src = "/icons/love-fill.svg"
-//         } else {
-//             btn.firstElementChild.src = "/icons/love-outline.svg"
-//         }
-//         urlInput.value = document.URL
-//         // alert(document.URL)
-//         document.forms["wish-form"].submit()
-//     })
-// })
 
 var wishBtns = document.querySelectorAll(".wish")
 var urlInput = document.getElementById("urlInput")
